267723b08c4873a9bc72d6c53cb636aeacc28783
mahadir04 <mahadirmrh04@gmail.com>
2023-12-07 03:48:38 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/lool/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lool
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Laptop List'),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          CardWidget('ACER Nitro-5', '4.5','nitro-5.jpg',
+              'Acer Nitro 5 AN515-44 15.6-inch Full HD Display Ryzen 5 8GB RAM 1TB HDD + 256GB SSD Gaming',13.5),
+          CardWidget('HP-Victus',  '3.8','Victus.jpg',
+              'HP VICTUS Gaming 15-FA0025 Core i5 12th Gen 15.6 Inch FHD Laptop - Vibe Gaming',15.6),
+          // Add more cards as needed
+        ],
+      ),
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  final String LaptopName;
+  final String Rating;
+  final String imageName;
+  final String description;
+  final double price;
+
+  CardWidget(this.LaptopName, this.Rating,this.imageName,this.description,this.price);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Image.asset('assets/images/$imageName',height: 150,width: 100,), // Replace with actual URL
+            title: Text(LaptopName),
+            //subtitle: Text($price),
+          ),
+          ListTile(
+            title: Text('Rating: $Rating'),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondPage(LaptopName,imageName,description,price)),
+              );
+            },
+            icon: Icon(Icons.shopping_cart),
+            label: Text('Buy Now'),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class SecondPage extends StatelessWidget {
+  final String LaptopName;
+  final String imageName;
+  final String description;
+  final double price;
+
+  SecondPage(this.LaptopName,this.imageName,this.description,this.price);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Buy Now'),
+        backgroundColor: Colors.green,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Product Name: $LaptopName\n'),
+            Image.asset('assets/images/$imageName'), // Replace with actual URL
+            //Text('\nAuthor Name: $authorName'), // Replace with actual author name
+            Text('\nShort Description: $description'),
+            Text('\nPrice: \$$price'), // Replace with actual price
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                  },
+                  child: Image.asset(
+                    "assets/images/bkash.png",
+                    height: 40,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+
+                  },
+                  child: Image.asset(
+                    "assets/images/nagad.png",
+                    height: 40,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+
+
+                  },
+                  child: Image.asset(
+                    "assets/images/rocket.png",
+                    height: 40,
+                  ),
+                ),
+              ],
+            ),
+            DropdownButton<String>(
+              items: ['Real copy', 'Paper print', 'Black & white print'].map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                // Handle dropdown selection
+              },
+              hint: Text('Select an option'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Go Back'),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
